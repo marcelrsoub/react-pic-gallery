819155328855c0891ef2eb3ab7f0d44c977509f5
Marcel Reis <marcelclemente.msc@gmail.com>
2021-01-04 00:17:07 +0100

    responsive gallery implemented

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,8 +3,29 @@ import React from 'react'
 import { ExampleComponent } from 'react-pic-gallery'
 import 'react-pic-gallery/dist/index.css'
 
+type imageObject = {
+  thumbnailSrc:string,
+  fullSrc:string
+}
+
+const listOfImages:imageObject[]=[
+  {thumbnailSrc:"https://picsum.photos/id/237/200/300",fullSrc:""},
+  {thumbnailSrc:"https://picsum.photos/id/154/200/150",fullSrc:""},
+  {thumbnailSrc:"https://picsum.photos/id/385/300/200",fullSrc:""},
+  {thumbnailSrc:"https://picsum.photos/id/25/100/250",fullSrc:""},
+  {thumbnailSrc:"https://picsum.photos/id/35/120/200",fullSrc:""},
+  {thumbnailSrc:"https://picsum.photos/id/84/280/185",fullSrc:""},
+  {thumbnailSrc:"https://picsum.photos/id/237/200/300",fullSrc:""},
+  {thumbnailSrc:"https://picsum.photos/id/154/200/150",fullSrc:""},
+  {thumbnailSrc:"https://picsum.photos/id/385/300/200",fullSrc:""},
+]
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div style={{background:'grey',padding:20,position:'absolute',width:'auto',height:'100%', border:'1px solid black'}}>
+      <ExampleComponent imgList={listOfImages} />
+    </div>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
+interface imageObject {
+  thumbnailSrc: string
+  fullSrc: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+interface Props {
+  imgList: imageObject[]
+}
+
+export const ExampleComponent = ({ imgList }: Props) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.grid}>
+        {imgList.map((element: imageObject) => {
+          return (
+            <div key={element.thumbnailSrc} className={styles.imgDiv}>
+              <div className={styles.innerImgDiv}>
+                <img src={element.thumbnailSrc} alt='' />
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
