eb3b5bf70b7e22efb71b8b5d9e693c313e7b3637
Marcel Reis-Soubkovsky <marcelclemente.msc@gmail.com>
2021-01-04 17:18:49 +0100

    Updates

---
+++ precache-manifest.8d0939c15bf97991352d8b10bb4aa748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963701f2f4cc9167f423d9f7789213c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46d44424654a9ffa14aa",
+    "url": "./static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "f500ad16851887840eb9",
+    "url": "./static/js/2.a3cfc1d2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.a3cfc1d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d44424654a9ffa14aa",
+    "url": "./static/js/main.a45421f5.chunk.js"
+  },
+  {
+    "revision": "52a92cf43cd6e9bc6740",
+    "url": "./static/js/runtime-main.732f0eaa.js"
+  }
+]);
