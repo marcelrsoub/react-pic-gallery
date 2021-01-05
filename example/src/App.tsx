7a306ef20a5d2105961a2182431414f358a4a18d
Marcel Reis-Soubkovsky <marcelclemente.msc@gmail.com>
2021-01-05 16:28:02 +0100

    new lightbox , download button and blob loading implemented

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PicGallery, { imageObject } from 'react-pic-gallery'
 
 const listOfImages:imageObject[]=[
-  {thumbnailSrc:"https://picsum.photos/id/237/200/300",fullSrc:"https://picsum.photos/id/237/800/600"},
+  {thumbnailSrc:"https://picsum.photos/id/237/200/300",fullSrc:"https://picsum.photos/id/237/800/600",description:"A Dog standing on a wooden floor"},
   {thumbnailSrc:"https://picsum.photos/id/154/200/150",fullSrc:"https://picsum.photos/id/154/200/150"},
   {thumbnailSrc:"https://picsum.photos/id/385/300/200",fullSrc:"https://picsum.photos/id/385/800/600"},
   {thumbnailSrc:"https://picsum.photos/id/25/100/250",fullSrc:"https://picsum.photos/id/25/100/250"},
@@ -15,7 +15,7 @@ const listOfImages:imageObject[]=[
 
 const App = () => {
   return (
-    <div style={{background:'grey',padding:20,position:'absolute',width:'auto',height:'100%', border:'1px solid black'}}>
+    <div style={{background:'#292929',padding:20,position:'absolute',width:'calc(100% - 35px)',height:'calc(100% - 35px)'}}>
       <PicGallery imgList={listOfImages} />
     </div>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 /* eslint-disable prettier/prettier */
 import * as React from 'react'
 import styled from 'styled-components'
+import { circularLoading } from '@yami-beta/react-circular-loading'
+
+// 1. Types Declaration
 
 export interface imageObject {
   fullSrc: string
-  thumbnailSrc: string
+  thumbnailSrc?: string
+  description?: string
 }
 
-interface Props {
-  imgList: imageObject[]
+export interface Options {
+  downloadBtnDisplay?: boolean
+  // shareBtnDisplay?:boolean
+  descriptionBoxDisplay?: boolean
 }
+
+// 2. Styled Components
 
 const ModalDiv = styled.div`
   position: fixed;
@@ -18,64 +26,216 @@ const ModalDiv = styled.div`
   width: 100%;
   height: 100%;
   background: rgba(0, 0, 0, 0.774);
+  /* backdrop-filter: blur(6px); */
   display: flex;
   z-index: 1e9;
   justify-content: center;
 `
 
+const LbButtonsDiv = styled.div`
+  position: absolute;
+  top: 10px;
+  width: calc(100% - 20px);
+`
+
+const LbButton = styled.a`
+  float: right;
+  text-decoration: none;
+  color: white;
+  padding: 5px;
+  border: 1px solid white;
+  border-radius: 5px;
+`
+
+const DescriptionDiv = styled.div`
+  background: white;
+  display:block;
+  margin:auto;
+  padding:10px;
+  width: calc(90% - 20px);
+`
+
 const Wrapper = styled.div({
   display: 'flex',
   width: '100%',
+  minWidth: 200,
   height: '100%'
 })
 
 const Grid = styled.div({
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
+  gap: '2px',
   width: '100%',
   height: 'auto'
 })
 
-const ImgModal = (props: { imgSrc: string; onClose: () => void }) => {
+// 3. Lightbox and Image Component
+
+const CircularLoading = circularLoading({
+  // Distance of dot from center `em`
+  distance: 1,
+  // Dot's size `em`
+  dotSize: 0.3,
+  // A number of dots
+  num: 10,
+  // Dot's color (base color)
+  dotColor0: `rgba(255, 255, 255, 0.1)`,
+  // Dot's color (semi highlight color)
+  dotColor1: `rgba(255 ,255, 255, 0.2)`,
+  // Dot's color (highlight color)
+  dotColor2: `rgba(255 ,255, 255, 1.0)`
+})
+
+const ImgLazyLoading = (props: {
+  imgSrc: string
+  style: React.CSSProperties
+  onClick: () => void
+}) => {
+  const [imgSrcUrl, setImgSrcUrl] = React.useState('')
+
+  React.useEffect(() => {
+    if (props.imgSrc) {
+      fetch(props.imgSrc)
+        .then((response) => response.blob())
+        .then((blob) => {
+          setImgSrcUrl(URL.createObjectURL(blob))
+        })
+    }
+  }, [])
+
+  if (imgSrcUrl) {
+    return (
+      <img
+        src={imgSrcUrl}
+        alt=''
+        style={{
+          ...props.style
+        }}
+        onClick={() => {
+          props.onClick()
+        }}
+      />
+    )
+  } else {
+    return (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          alignContent: 'center',
+          justifyContent: 'center',
+          background: 'rgba(0, 0, 0, 0.157)',
+          border: '1px solid rgba(0, 0, 0,0.3)'
+        }}
+      >
+        <CircularLoading />
+      </div>
+    )
+  }
+}
+
+const ImgLightbox = (props: {
+  imgObj: imageObject | null
+  options?: Options
+  onClose: () => void
+}) => {
+  const [imgSrcUrl, setImgSrcUrl] = React.useState('')
+
+  React.useEffect(() => {
+    const imgSrc = props.imgObj?.fullSrc
+      ? props.imgObj.fullSrc
+      : props.imgObj?.thumbnailSrc
+      ? props.imgObj.thumbnailSrc
+      : null
+    if (imgSrc) {
+      fetch(imgSrc)
+        .then((response) => response.blob())
+        .then((blob) => {
+          setImgSrcUrl(URL.createObjectURL(blob))
+        })
+    }
+  }, [])
+
   return (
     <ModalDiv
       onClick={() => {
         props.onClose()
       }}
     >
-      <img
-        src={props.imgSrc}
-        alt=''
-        style={{
-          touchAction: 'pan-right pinch-zoom',
+      {/* <LbButton ><a href={props.imgSrc} download>Download</a></LbButton> */}
+      <LbButtonsDiv>
+        <LbButton href={imgSrcUrl} download>
+          Download
+        </LbButton>
+      </LbButtonsDiv>
+
+      {imgSrcUrl === '' ? (
+        <div style={{ display: 'block', margin: 'auto' }}>
+          <CircularLoading />
+        </div>
+      ) : (
+        <div style={{
           display: 'block',
           margin: 'auto',
           maxWidth: '90%',
           maxHeight: '80%'
-        }}
-      />
+        }}>
+          <img
+            src={imgSrcUrl}
+            alt=''
+            style={{
+              touchAction: 'pan-right pinch-zoom',
+              display: 'block',
+              margin: 'auto',
+              maxWidth: '90%',
+              maxHeight: '80%'
+            }}
+          />
+          {props.options?.descriptionBoxDisplay ? (props.imgObj?.description ? <DescriptionDiv>{props.imgObj?.description}</DescriptionDiv> : null) : null}
+        </div>
+      )}
     </ModalDiv>
   )
 }
 
-const PicGallery = ({ imgList }: Props) => {
+// 4. Main Component
+
+// 4.1. Default Options
+
+const defaultOptions:Options = {
+  downloadBtnDisplay:true,
+  descriptionBoxDisplay:true,
+}
+
+// 4.2. Main
+
+const PicGallery = (props : { imgList: imageObject[], options?:Options }) => {
   const [open, setOpen] = React.useState(false)
-  const [modalImgSrc, setModalImgSrc] = React.useState('')
+  const [modalImgObj, setModalImgObj] = React.useState<imageObject | null>(null)
+
+  const options:Options = { ...defaultOptions, ...props.options}
+
   return (
     <Wrapper>
       {open ? (
-        <ImgModal
-          imgSrc={modalImgSrc}
+        <ImgLightbox
+          imgObj={modalImgObj}
+          options={options}
           onClose={() => {
             setOpen(false)
           }}
         />
       ) : null}
       <Grid>
-        {imgList.map((element: imageObject) => {
+        {props.imgList.map((element: imageObject) => {
           return (
             <div
-              key={element.thumbnailSrc}
+              key={
+                element.thumbnailSrc ? element.thumbnailSrc : element.fullSrc
+              }
               className='imgDiv'
               style={{
                 overflow: 'hidden',
@@ -96,16 +256,19 @@ const PicGallery = ({ imgList }: Props) => {
                   cursor: 'pointer'
                 }}
               >
-                <img
-                  src={element.thumbnailSrc}
-                  alt=''
+                <ImgLazyLoading
+                  imgSrc={
+                    element.thumbnailSrc
+                      ? element.thumbnailSrc
+                      : element.fullSrc
+                  }
                   style={{
                     flexShrink: 0,
                     minWidth: '100%',
                     minHeight: '100%'
                   }}
                   onClick={() => {
-                    setModalImgSrc(element.fullSrc)
+                    setModalImgObj(element)
                     setOpen(!open)
                   }}
                 />
