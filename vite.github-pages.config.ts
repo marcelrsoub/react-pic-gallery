54f607246f546b920226d5662c658c56aa3e0b68
Marcel Reis Soubkovsky <marcelclemente.msc@gmail.com>
2021-12-30 12:04:24 +0100

    Update vite.github-pages.config.ts

--- vite.github-pages.config.ts
+++ vite.github-pages.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://marcelrsoub.github.io/react-pic-gallery/',
   build: {
     outDir: 'build'
   }
