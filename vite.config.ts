4f218b01c00c1136f1966a738633443c8719e1d6
Marcel Reis <marcelclemente.msc@gmail.com>
2022-05-29 09:51:21 +0200

    Types declared in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    dts({
+        insertTypesEntry: true,
+    }),],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/lib/main.tsx'),
